44ed300f854c3e366f6112fed445dab842669d33
Fabian Losada <fabianlosada1@hotmail.com>
2024-06-24 11:59:59 -0400

    numeros primo

---
+++ Ejercicio N#5_Buscador de Números Primo/02_NumPrimo/02_NumPrimo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static bool EsPrimo(int numero)
+    {
+        if (numero <= 1)
+            return false;
+
+        for (int i = 2; i * i <= numero; i++)
+        {
+            if (numero % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Escribe el inicio del rango: ");
+        int inicio = int.Parse(Console.ReadLine());
+
+        Console.Write("Escribe el fin del rango: ");
+        int fin = int.Parse(Console.ReadLine());
+
+        List<int> primosEncontrados = new List<int>();
+
+        for (int num = inicio; num <= fin; num++)
+        {
+            if (EsPrimo(num))
+                primosEncontrados.Add(num);
+        }
+        Console.WriteLine("numeros primos hayados en el rango:");
+        foreach (var primo in primosEncontrados)
+        {
+            Console.Write(primo + " ");
+        }
+    }
+}
